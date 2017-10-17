f83a867329e35850af004eae4d4b0bb11d6d5771
beyfea <31735304+beyfea@users.noreply.github.com>
2017-10-16 23:53:51 -0400

    Delete GC_calculator_ChenWang.py

--- GC_calculator_ChenWang.py
+++
@@ -1,8 +0,0 @@
-
-#Paste Code here
-flu_ns1_seq = 'GTGACAAAGACATAATGGATCCAAACACTGTGTCAAGCTTTCAGGTAGATTGCTTTCTTTGGCATGTCCGCAAACGAGTTGCAGACCAAGAACTAGGTGA'
-c = flu_ns1_seq.count('C')
-g = flu_ns1_seq.count('G')
-cg = c + g
-Percentage = cg / len(flu_ns1_seq)
-print(Percentage)
